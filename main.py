610a8285ebadc3d1f6331d5bb27bd394073bc82a
Matt Guenther <mguenther006@users.noreply.github.com>
2018-02-07 13:45:30 -0600

    Initial Commit
    Pushing Initial Files

---
+++ main.py
@@ -0,0 +1,23 @@
+import requests
+import json
+from prettyprinter import cpprint, set_default_config
+from apiVars import *
+
+# ------------------------- Authentication --------------------------------
+# API Call to generate and store session token
+payload = {'username': apiUsername, 'password': apiPass}
+sessionToken = requests.post('https://cloud.tenable.com/session', data=json.dumps(payload))
+tokenResponse = sessionToken.json()
+
+# Final parsed token for use
+tokenParsed = tokenResponse['token']
+print(tokenParsed)
+
+# Construct the headers needed for api calls
+header = {'X-Cookie': 'token='+tokenParsed}
+
+# ------------------------------ Get Scans ---------------------------
+scanID = input("Enter scan ID \n") # Ex: 601 is SOC Agent Scan
+getScanInfo = requests.get('https://cloud.tenable.com/scans/'+scanID, headers=header)
+scannerResponse = getScanInfo.json()
+cpprint(scannerResponse)
